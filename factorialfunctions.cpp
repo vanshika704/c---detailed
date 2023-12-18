5c6b3776386a9d803f8daf3e310487ee7e42d2a9
vanshika704 <sharmavanshi704@gmail.com>
2023-12-18 23:06:19 +0530

    factorial

---
+++ factorialfunctions.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int factor(int number1){
+int number1;int i;int factorial;
+ for( i=2;i<number1;i++){
+int factorial=number1*i;
+}
+}
+int main(){int number1;int i;int factorial;
+    cout<<"enter number 1";
+    cin>>number1;
+    int ans=factor(number1);
+
+
+   
+    cout<< factorial<<endl;
+    return 0;
+}

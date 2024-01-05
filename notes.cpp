da4a7fa897d1ee5ca1228a7b8ca36686a721aea2
vanshika704 <sharmavanshi704@gmail.com>
2024-01-05 17:06:52 +0530

    notes

--- notes.cpp
+++ notes.cpp
@@ -13,6 +13,7 @@ int main() {
         default:
             result += amount / 2000;
             amount %= 2000;
+            cout<< "2000 rupee notes are:"<<amount/2000<<endl;
     }
 
     switch (amount / 500) {
@@ -21,6 +22,7 @@ int main() {
         default:
             result += amount / 500;
             amount %= 500;
+             cout<< "500 rupee notes are:"<<amount/500<<endl;
     }
 
     switch (amount / 100) {
@@ -29,6 +31,7 @@ int main() {
         default:
             result += amount / 100;
             amount %= 100;
+            cout<< "100 rupee notes are:"<<amount/100<<endl;
     }
 
     switch (amount / 50) {
@@ -37,6 +40,7 @@ int main() {
         default:
             result += amount / 50;
             amount %= 50;
+            cout<< "50 rupee notes are:"<<amount/50<<endl;
     }
 
     cout << "Result: " << result << " notes" << endl;

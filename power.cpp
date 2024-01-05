8e66273f255295c61301912d5f898c8f25a17346
vanshika704 <sharmavanshi704@gmail.com>
2024-01-05 17:43:37 +0530

    commit

---
+++ power.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<" enter a";
+    cin>> a;
+    cout<<" enter b";
+    cin>> b;
+    int result= pow(a,b);
+    cout<< result<< endl;
+}
+
+
+
+

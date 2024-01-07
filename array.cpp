15ab81309476510052012704d8c1402ac9401bc7
vanshika704 <sharmavanshi704@gmail.com>
2024-01-07 21:09:54 +0530

    array

--- array.cpp
+++ array.cpp
@@ -23,7 +23,7 @@ cout<<" enter number of elements of array";
 cin>> size;
  cout<< " enter the elements of array";
  int num[15];
- for ( i=0;i<=n; i++){
+ for ( i=0;i<=size; i++){
     cin>> num[i];
  }
 return 0;
---
+++ sumarray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){int i,size;
+cout<<" enter number of elements of array";
+cin>> size;
+ cout<< " enter the elements of array";
+ int num[15];
+ for ( i=0;i<=size; i++){
+    cin>> num[i];
+ }
+return 0;
+ 
+}

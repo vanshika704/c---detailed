571f721a5288e3c011c3b868cbfc3f69ced9d62f
vanshika704 <sharmavanshi704@gmail.com>
2024-01-07 19:50:31 +0530

    array

--- power.cpp
+++ power.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include <math.h>
 using namespace std;
+int power(int a,int b );
+
 int main(){
     int a,b;
     cout<<" enter a";
     cin>> a;
     cout<<" enter b";
     cin>> b;
-    int result= pow(a,b);
+   
     cout<< result<< endl;
 }
 

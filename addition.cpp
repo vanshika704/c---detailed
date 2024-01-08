2947eb353e3de648aaf1829b50d15df85595e1e5
Vanshika Sharma <143260941+vanshika704@users.noreply.github.com>
2024-01-08 22:25:43 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){ int amount1;int amount2;int sum;
     cout<<"enter amount1";
-    cin>> amount1;//..
+    cin>> amount1;
     cout<<"enter amount2";
     cin>> amount2;
     sum=amount1+amount2;
